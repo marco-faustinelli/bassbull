751a6660251c84bf210a8d453082b99998bb3ec9
Marco Faustinelli <marco.faustinelli@neomobile.com>
2015-11-28 07:52:15 +0100

    pointfree really now

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Foldable as F
+import qualified Data.Vector as V
 -- from cassava
-import Data.Csv.Streaming
+import Data.Csv
 
 -- a simple type alias for data
 type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
@@ -11,13 +11,13 @@ type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 fourth :: (a, b, c, d) -> d
 fourth (_, _, _, d) = d
 
-baseballStats :: BL.ByteString -> Records BaseballStats
+baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
 baseballStats = decode NoHeader
 
 tain :: IO ()
 main = do
   csvData <- BL.readFile "batting.csv"
-  let summed = F.foldr summer 0 $ baseballStats csvData
+  let summed = fmap (V.foldl summer 0) $ baseballStats csvData
   putStrLn $ "Total atBats was: " ++ (show summed)
   where summer = (+) . fourth
 
