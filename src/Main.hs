87272e685c158a360da9a05b8e52a281ab6ac99c
Marco Faustinelli <marco.faustinelli@neomobile.com>
2015-11-28 08:14:27 +0100

    created program as library with slim Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,9 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
--- from cassava
-import Data.Csv
+import Bassbull
 
--- a simple type alias for data
-type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
-
-fourth :: (a, b, c, d) -> d
-fourth (_, _, _, d) = d
-
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
-baseballStats = decode NoHeader
-
-tain :: IO ()
+main :: IO ()
 main = do
-  csvData <- BL.readFile "batting.csv"
-  let summed = fmap (V.foldl summer 0) $ baseballStats csvData
+  summed <- getAtBatsSum "batting.csv"
   putStrLn $ "Total atBats was: " ++ (show summed)
-  where summer = (+) . fourth
 
